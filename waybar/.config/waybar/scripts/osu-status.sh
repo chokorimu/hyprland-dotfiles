9b87f0e8caf002c89f3c9a3d4467dacb1f84edec
chokorimu <rezondavar@gmail.com>
2025-12-23 23:58:38 +0700

    little update

--- waybar/.config/waybar/scripts/osu-status.sh
+++ waybar/.config/waybar/scripts/osu-status.sh
@@ -7,10 +7,17 @@ TITLE=$(echo "$WINDOW" | jq -r '.title')
 
 if [[ "$CLASS" =~ "osu" ]]; then
     CLEAN_TITLE=$(echo "$TITLE" | sed 's/ - osu!$//;s/^osu! - //')
+    
     if [ "$CLEAN_TITLE" = "osu!" ]; then
-        echo " Selecting Beatmaps..."
+        jq -n --unbuffered --compact-output \
+            --arg text " Selecting a beatmap..." \
+            --arg class "selecting" \
+            '{text: $text, class: $class}'
     else
-        echo " $CLEAN_TITLE"
+        jq -n --unbuffered --compact-output \
+            --arg text " $CLEAN_TITLE" \
+            --arg class "playing" \
+            '{text: $text, class: $class}'
     fi  
 else
     echo ""
